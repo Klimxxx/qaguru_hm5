c928c2c5f880ad72844b8f1427d6c0c5a0c89de0
Klim Trotsenko <klim.trotsenko7@gmail.com>
2023-08-10 08:51:33 +0300

    update homework

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,11 +1,7 @@
 from selene import be, have, browser
-import pytest
 
 
 def test1():
-    browser.config.hold_driver_at_exit = True
-
-
     browser.open('/automation-practice-form')
     # вводим имя, фамилию и имейл
     browser.element('[id="firstName"]').should(be.blank).type('Klim')
@@ -14,17 +10,16 @@ def test1():
     # выбираем пол
     browser.element('[for="gender-radio-1"]').click()
     # вводим номер телефона
-    browser.element('[id="userNumber"]').should(be.blank).type('89876543210')#.press_tab()  #.press_enter()
+    browser.element('[id="userNumber"]').should(be.blank).type('89876543210')
+    # нажимаем на поле выбора даты дня рождения
+    browser.element('#dateOfBirthInput').click()
     # выбираем год
-    browser.element('.react-datepicker__year-select').click().element('[value="1989"]').click()
+    browser.element('[class^=react-datepicker__year-select]').click().element('[value="1989"]').click()
     # выбираем месяц
     browser.element('.react-datepicker__month-select').click().element('[value="0"]').click()
     # выбираем день
     browser.element('[aria-label = "Choose Monday, January 2nd, 1989"]').click()
-
-
-
-
-
+    # подтверждаем номер телефон
+    browser.element('[id="userNumber"]').click().press_enter()
+    # проверяем успешность регистрации
     browser.element('#example-modal-sizes-title-lg').should(have.text("Thanks for submitting the form"))
-
